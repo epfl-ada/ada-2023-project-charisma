ba1ff22acc4c0738aaa766296f7bbbed5346c602
Anand Choudhary <79297451+andy-joy-25@users.noreply.github.com>
2023-11-17 23:40:06 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+MAIN_DATA_PATH = r"/Users/anandchoudhary/Library/Mobile Documents/com~apple~CloudDocs/ANAND_EPFL/1st Sem,1st Year/CS-401/Main_Data" #Path to your Data Folder (CMU Movie Summary Corpus)
+SPOTIFY_CLIENT_ID = ""#Your SPOTIFY_CLIENT_ID
+SPOTIFY_CLIENT_SECRET = ""#Your SPOTIFY_CLIENT_SECRET
