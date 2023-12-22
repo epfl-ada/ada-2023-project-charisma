79e5bc9f56fb5900bdd89fce4f9c8b43f877da09
Anand Choudhary <79297451+andy-joy-25@users.noreply.github.com>
2023-12-22 23:42:41 +0100

    final commit!

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-MAIN_DATA_PATH = r"/Users/anandchoudhary/Library/Mobile Documents/com~apple~CloudDocs/ANAND_EPFL/1st Sem,1st Year/CS-401/Main_Data" #Path to your Data Folder
+MAIN_DATA_PATH = r"" #Path to your Data Folder
 SPOTIPY_CLIENT_ID = ""#Your SPOTIPY_CLIENT_ID
 SPOTIPY_CLIENT_SECRET = ""#Your SPOTIPY_CLIENT_SECRET
 
